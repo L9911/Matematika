8c322d3b373dcf51ed6d502d2cd4ff453d1bb53a
L9911 <renleeuwgabriel@gmail.com>
2024-11-24 15:53:14 +0700

    Update MatematikaCanggihBeraksi.java

--- MatematikaCanggihBeraksi.java
+++ MatematikaCanggihBeraksi.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package matematika;
-
-/**
- *
- * @author Ghaxre
- */
 public class MatematikaCanggihBeraksi {
     public static void main(String[] args) {
         MatematikaCanggih mtkCanggih = new MatematikaCanggih();
